bee349c356f4febe6b711ecc08bd864c60cc1df5
Alfred Chen <brilychen@gmail.com>
2018-09-21 17:29:54 +0800

    add 2 Env

---
+++ src/main/java/com/example/concurrency/ConcurrencyTest.java
@@ -0,0 +1,46 @@
+package com.example.concurrency;
+
+import com.example.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+@NotThreadSafe
+public class ConcurrencyTest {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    private static void add(){
+        count++;
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        // 定义线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++){
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+}
